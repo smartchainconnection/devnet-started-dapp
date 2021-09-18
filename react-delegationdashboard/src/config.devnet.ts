99a341b8186b4d3bdd016f4da8514393d3290812
Smart Chain Connection <contact@smartchainconnection.com>
2021-09-17 17:02:37 -0700

    dev changes with the new SC

--- react-delegationdashboard/src/config.devnet.ts
+++ react-delegationdashboard/src/config.devnet.ts
@@ -39,7 +39,7 @@ export const network: NetworkType = {
   apiAddress: 'https://devnet-api.elrond.com',
   gatewayAddress: 'https://devnet-gateway.elrond.com',
   explorerAddress: 'http://devnet-explorer.elrond.com/',
-  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqy8lllls62y8s5',
+  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzhllllsp9wvyl',
 };
 
 const networkSchema = object({
